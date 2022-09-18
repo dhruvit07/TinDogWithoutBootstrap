b402848e5169a6a3077d5e3183f4b294615e6f75
Dhruvit Salat <salatd0852@gmail.com>
2022-09-18 18:10:57 +0530

    initial Commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("open").onclick = function () {
+    document.getElementById("sidenav").style.width = "250px";
+};
+
+document.getElementById("close").onclick = function () {
+    document.getElementById("sidenav").style.width = "0";
+};
